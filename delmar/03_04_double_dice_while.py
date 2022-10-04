796717b092bad810c4ed3cf37df9412d024e7cba
odaadvanced <stbaoda@gmail.com>
2022-10-04 15:59:24 -0500

    class files

---
+++ delmar/03_04_double_dice_while.py
@@ -0,0 +1,15 @@
+#03_04_double_dice_while
+import random
+throw_1 = random.randint(1, 6)
+throw_2 = random.randint(1, 6)
+count = 1
+while not (throw_1 == 6 and throw_2 == 6):
+	total = throw_1 + throw_2
+	count = count + 1
+	print(total)
+	throw_1 = random.randint(1, 6)
+	throw_2 = random.randint(1, 6)
+
+print ('finished')
+print (count)
+print('Double Six thrown!')
---
+++ delmar/test1.py
@@ -0,0 +1,9 @@
+total = 3
+if total >= 5 and total <= 9:
+    print('not as bad')
+elif total == 3:
+    print ('very low number')
+else:
+    print ('not good')
+    
+print ('the end')
