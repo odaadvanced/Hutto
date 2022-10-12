4a11ca9de60e869e08f6a3b75bb5d014f2b7cdd8
odaadvanced <stbaoda@gmail.com>
2022-10-12 15:31:23 -0500

    commit

---
+++ Katherine/4_2_polite_function.py
@@ -0,0 +1,5 @@
+#4_2_polite_function
+def make_polite(sentence):
+    polite_sentence = sentence + ' please'
+    return polite_sentence
+print(make_polite('Pass the salt')) 
---
+++ Katherine/4_3_hello_n.py
@@ -0,0 +1,5 @@
+#4_3_hello(n)
+def say_hello(n):
+    for x in range (0, n):
+        print ('Hello')
+say_hello(5)
