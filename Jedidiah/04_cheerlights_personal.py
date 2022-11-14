bff0448024cbf3dbf47f389b7df30dc0e53fc7e0
odaadvanced <stbaoda@gmail.com>
2022-11-14 12:20:42 -0600

    message

---
+++ Jedidiah/04_cheerlights_personal.py
@@ -0,0 +1,28 @@
+# 04_cheerlights.py
+# From the code for the Box 1 kit for the Raspberry Pi by MonkMakes.com
+
+from gpiozero import Button, RGBLED
+from colorzero import Color
+import time, requests
+
+update_period = 10 # seconds
+led = RGBLED(red=18, green=23, blue=24)
+button = Button(25)
+
+cheerlights_url = "http://api.thingspeak.com/channels/1417/field/2/last.txt"
+old_color = None
+
+def pressed():
+    led.color = Color(0, 0, 0)  # LED off
+button.when_pressed = pressed
+
+while True:
+    try:
+        cheerlights = requests.get(cheerlights_url)
+        color = cheerlights.content     # the color as text
+        if color != old_color:
+            led.color = Color(color)    # the color as an object
+            old_color = color           
+    except Exception as e:
+        print(e)
+    time.sleep(update_period)  # don't flood the web service
