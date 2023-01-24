043880db57daf71ebb69cdd2e04374a91b4197ad
odaadvanced <stbaoda@gmail.com>
2023-01-24 15:50:28 -0600

    hi

---
+++ ellieballard/chapter_4/exponent.py
@@ -0,0 +1,5 @@
+response1 = input("Enter a base: ")
+response2 = input("Enter an exponent: ")
+operation = float(response1) ** float(response2)
+print (str(response1) + " to the power of " + str(response2) + " = " + str(operation))
+
