0a5b3952915d3c695e79daaa1f4b29e58a3e58bd
odaadvanced <stbaoda@gmail.com>
2023-04-11 10:01:04 -0500

    commit

--- Katherine/budget_project.py
+++ Katherine/budget_project.py
@@ -4,8 +4,17 @@ class budget:
         self.clothing = clothing
         self.entertainment = entertainment
         
-    def spend (total, cost):
-        self.spend = self.total - self.cost
-        print(spend)
+    def spend (self, cost):
+        self = self - self.cost
+        print(self)
     
+    def deposit(category, amount):
+        self.category += amount
+        
+    def transfer(category_1, category_2, transfer):
+        category_1 += transfer
+        category_2 -= transfer
     
+    def percent_of_total_expenses(item, spend, total):
+        percent = (spend/total) * 100
+        print(f"{percent} of total expenses spent on {item}")
