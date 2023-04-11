a64d1bba5aed35f8daf6e18bd26ef94ff8c60064
odaadvanced <stbaoda@gmail.com>
2023-04-11 13:30:39 -0500

    message

--- Jedidiah/Chapter 12/testing_program.py
+++ Jedidiah/Chapter 12/testing_program.py
@@ -1,7 +1,3 @@
-spent_for_food = 0
-spent_for_clothing = 0
-spent_for_entertainment = 0
-
 class Budget:
     def __init__(self, food, clothing, entertainment):
         self.food = food
@@ -13,26 +9,23 @@ class Budget:
 Amount for clothing: ${self.clothing}
 Amount for entertainment: ${self.entertainment}''')
         
-    def spend(self, category1, category2, category3):
-        global spent_for_food
-        global spent_for_clothing
-        global spent_for_entertainment
-        self.food = self.food - category1
-        self.clothing = self.clothing - category2
-        self.entertainment = self.entertainment - category3
-        spent_for_food += category1
-        spent_for_clothing += category2
-        spent_for_entertainment += category3
-        print(f'''You spent ${category1} on food, ${category2} on clothing,
-and ${category3} on entertainment.''')
-        return (spent_for_food, spent_for_clothing, spent_for_entertainment)
- 
-    def deposit(self, category1, category2, category3):
-        self.food += category1
-        self.clothing += category2
-        self.entertainment += category3
-        print(f'''You deposited ${category1} for food, ${category2} for
-clothing, and ${category3} for entertainment.''')
+    def spend(self, category, amount):
+        if category == "food":
+            self.food = self.food - amount
+        elif category == "clothing":
+            self.clothing = self.clothing - amount
+        else:
+            self.entertainment = self.entertainment - amount
+        print(f'You spent ${amount} on {category}.')
+        
+    def deposit(self, category, amount):
+        if category == "food":
+            self.food += amount
+        elif category == "clothing":
+            self.clothing += amount
+        else:
+            self.entertainment += amount
+        print(f'You deposited ${amount} for {category}.')
     
     def compute(self, category):
         if category ==1:
@@ -44,40 +37,76 @@ clothing, and ${category3} for entertainment.''')
     
     def transfer(self, transfer_from, transfer_to, amount):
         '''Transfers a certain amount from category 1 to category 2.'''
-        if transfer_from == 1:
+        if transfer_from == 'food':
             self.food = self.food - amount
-            if transfer_to == 2:               
+            if transfer_to == 'clothing':               
                 self.clothing += amount
             else:
                 self.entertainment += amount
-        elif transfer_from == 2:
+        elif transfer_from == 'clothing':
             self.clothing = self.clothing - amount
-            if transfer_to == 1:
+            if transfer_to == 'food':
                 self.food += amount
             else:
                 self.entertainment += amount                
         else:
             self.entertainment = self.entertainment - amount
-            if trannsfer_to ==1:
+            if transfer_to == 'food':
                 self.food += amount
             else:
                 self.clothing += amount
                 
     def budget_calculations(self, category):        
-        food_spent = spend[0]
-        clothing_spent = spend[1]
-        entertainment_spent = spend[2]
+        '''Budget_calculations calculates what percent each item was of the original.'''
+        food_spent = food_original
+        clothing_spent = clothing_original
+        entertainment_spent = entertainment_original
         total_spent = food_spent + clothing_spent + entertainment_spent
-        if category == 1:
-            money = food_spent
-            category_from = food
-        elif category == 2:
+        if category == 'food':
+            money = food_spent            
+        elif category == 'clothing':
             money = clothing_spent
-            category_from = clothing
         else:
             money = entertainment_spent
-            category_from = entertainment 
-        percent = (money/total_spent) * 100
-        print(f'You are spending {percent} % of your budget on {category_from}.')
-
+        try:
+            percent = (money/total_spent) * 100
+            print(f'You are spending {percent}% of your budget on {category}.')
+        except ZeroDivisionError:
+            print(f'You are spending 0% of your budget on {category}.')
+    
+    def budget_current_calculations(self, category):
+        total_spent = myBudget.food + myBudget.clothing + myBudget.entertainment
+        if total_spent > 0:
+            if category == "food":
+                percent = self.food / total_spent * 100
+            elif category == "clothing":
+                percent = self.clothing / total_spent * 100
+            else:
+                percent = self.entertainment / total_spent * 100
+            print(f'{percent} % of your budget is for {category}.')
+        else:
+            print(f'0% of your budget is for {category}.')
+            
+    def budget_spent_calculations(self, category):
+        global food_original
+        global clothing_original
+        global entertainment_original
+        spent_on_food = food_original - self.food
+        spent_on_clothing = clothing_original - self.clothing
+        spent_on_entertainment = entertainment_original - self.entertainment
+        total_spent = spent_on_food + spent_on_clothing + spent_on_entertainment
+        if total_spent > 0:
+            if category == "food":
+                percent = spent_on_food/total_spent * 100
+            elif category == "clothing":
+                percent = spent_on_clothing/total_spent * 100
+            else:
+                percent = spent_on_entertainment/total_spent * 100
+            print(f'Your spending on {category} is {percent}% of your total spending so far.')
+        else:
+            print(f'Your spending on {category} is 0% of your total spending so far.')
+            
 myBudget = Budget(3500, 1000, 500)
+food_original = myBudget.food
+clothing_original = myBudget.clothing
+entertainment_original = myBudget.entertainment
