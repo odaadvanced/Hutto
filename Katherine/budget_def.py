7ab3a4cbb98aed48bb4dc7b9c91b50b50b6e84df
odaadvanced <stbaoda@gmail.com>
2023-04-11 13:30:15 -0500

    commit

---
+++ Katherine/budget_def.py
@@ -0,0 +1,34 @@
+#def for budget_project.py
+
+#def original(self, original):
+original_food = 40
+original_clothing = 100
+original_entertainment = 140
+    
+def spend (self, cost):
+    self.amount = self.amount - cost
+    print(f"${self.amount} remaining")
+
+def food(budget):
+    pass
+def food_total (self, original):
+    return original_food
+def food_cost (self, cost):
+    cost = original_food - self.amount
+    return cost
+
+def clothing(budget):
+    pass 
+def clothing_total(self, original):
+    return original_clothing
+def clothing_cost(self, cost):
+    cost = original_clothing - self.amount
+    return cost
+
+def entertainment(budget):
+    pass
+def entertainment_total(self, original):
+    return original_entertainment
+def entertainment_cost(self, cost):
+    cost = original_entertainment - self.amount
+    return cost
--- Katherine/budget_project.py
+++ Katherine/budget_project.py
@@ -1,20 +1,54 @@
+import budget_def
+
 class budget:
-    def __init__(self, food, clothing, entertainment):
-        self.food = food
-        self.clothing = clothing
-        self.entertainment = entertainment
+    def __init__(self, amount):
+        self.amount = amount
+    
+    def original(self, original):
+        original_food = input(f"How much money did you start with in your food category?")
+        original_clothing = input(f"How much money did you start with in your clothing category?")
+        original_entertainment = input(f"How much money did you start with in your entertainment category?")
+        return original_food, original_clothing, original_entertainment
         
     def spend (self, cost):
-        self = self - self.cost
-        print(self)
+        self.amount = self.amount - cost
+        print(f"${self.amount} remaining")
     
-    def deposit(category, amount):
-        self.category += amount
-        
-    def transfer(category_1, category_2, transfer):
-        category_1 += transfer
-        category_2 -= transfer
+    def deposit(self, deposit):
+        self.amount += deposit
+        print(f"${self.amount} have been allocated to this category in total")
     
-    def percent_of_total_expenses(item, spend, total):
-        percent = (spend/total) * 100
-        print(f"{percent} of total expenses spent on {item}")
+    def transfer(self, transfer):
+        category_1 = input("From what category do you want to withdraw money?")
+        category_1_amount = input(f"Enter an integer to represent the amount of money in {category_1}:")
+        transfer = int(input(f"How much money do you want to withdraw from {category_1}?"))
+        category_2 = input("To what category do you want to transfer funds?")
+        category_2_amount = int(input(f"Enter an integer to represent the amount of money in {category_2}"))
+        new_category_1_amount = int(category_1_amount) - int(transfer)
+        new_category_2_amount = int(category_2_amount) + int(transfer)
+        print(f"The category of {category_1} now contains ${new_category_1_amount}")
+        print(f"The category of {category_2} now contains ${new_category_2_amount}")
+    
+    def percent_of_total_expenses(self, cost_food, cost_clothing, cost_entertainment):
+        cost_food = budget_def.food_cost(self, cost_food)
+        cost_clothing = budget_def.clothing_cost(self, cost_clothing)
+        cost_entertainment = budget_def.entertainment_cost(self, cost_entertainment)
+        total_expenses = int(cost_food) + int(cost_clothing) + int(cost_entertainment)
+        category = input("Pick a category of your budget. Are you interested in the cost of food, clothing, or entertainment?")
+        if category == 'food':
+            expense = cost_food
+        elif category == 'clothing':
+            expense = cost_clothing
+        elif category == 'entertainment':
+            expense = cost_entertainment
+        percent = expense/total_expenses * 100
+        print(f"{percent}% of total expenses spent on {category}")
+
+class food(budget):
+    pass
+
+class clothing(budget):
+    pass
+
+class entertainment(budget):
+    pass
