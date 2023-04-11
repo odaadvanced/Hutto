073ac375f48fa6fd1d28ca25e55a41db10d20339
odaadvanced <stbaoda@gmail.com>
2023-04-11 15:59:23 -0500

    commit

---
+++ Katherine/chapter_11_and_12/main.py
@@ -0,0 +1,4 @@
+# main.py
+import adder as ad
+value = ad.add(2, 2)
+print(value)
---
+++ Katherine/packages/main.py
@@ -0,0 +1,7 @@
+# main.py
+import mypackages.module1, mypackages.module2
+mypackages.module1.greet("Katherine")
+mypackages.module2.depart("Katherine")
+#import adder as ad
+#value = ad.add(2, 2)
+#print(value)
