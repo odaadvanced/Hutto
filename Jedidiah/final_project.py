0f50b2f5caf684dcb9e558dd0b418f47c69a4a5f
odaadvanced <stbaoda@gmail.com>
2023-05-08 15:42:01 -0500

    message

--- Jedidiah/final_project.py
+++ Jedidiah/final_project.py
@@ -3,39 +3,48 @@ import sys
 import time
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../../')))
 from sphero_sdk import SpheroRvrObserver
-from sphero_sdk import DriveFlagsBitmask
+from sphero_sdk import SpheroRvrAsync
+from sphero_sdk import SerialAsyncDal
 from oled_io import Oled_io
-import photoresistor
-import color_detection
-import RPi.GPIO as GPIO
+#import photoresistor
+#import color_detection
+#import RPi.GPIO as GPIO
 import asyncio
 import random
 
 display = Oled_io()
-rvr = SpheroRvrObserver()
+#rvr = SpheroRvrObserver()
+loop = asyncio.get_event_loop()
+rvr = SpheroRvrAsync(
+    dal=SerialAsyncDal(
+        loop
+    )
+)
 
 def display_speed():
-    speed = random.randint(0,30)    
-    return speed
+    speed2 = random.randint(0,30)    
+    return speed2
 
-   
-while True:
-    try:        
-        rvr.wake()
-        time.sleep(2)
-        rvr.reset_yaw()
-        time.sleep(2)
+async def main():
+    await rvr.wake()
+    await rvr.reset_yaw()
+    await asyncio.sleep(.5)   
+    while True:            
         new_speed = display_speed()
         display.print(str(new_speed))
-        rvr.drive_with_heading(
-            speed=new_speed,  # Valid speed values are 0-255
-            heading=90,  # Valid heading values are 0-359
-            flags=DriveFlagsBitmask.none.value
-        )
-    
-    except KeyboardInterrupt:
-        print('Program terminated by keyboard interrupt.')
+        await rvr.drive_with_heading(new_speed,0,0)
+        await asyncio.sleep(2)
         
+try:
+    loop.run_until_complete(
+        asyncio.gather(
+            main()
+        )
+    )
+    
+except KeyboardInterrupt:
+    print('Program terminated by keyboard interrupt.')
+    GPIO.cleanup()    
    # finally:
     #    rvr.close()
     
