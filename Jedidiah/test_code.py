a6dc460fd8aa2df7d97983847925d18d8af0b560
odaadvanced <stbaoda@gmail.com>
2023-05-08 19:14:42 -0500

    m

--- Jedidiah/test_code.py
+++ Jedidiah/test_code.py
@@ -1,12 +1,28 @@
-#test code
-import random
-from oled_io import Oled_io
-display = Oled_io()
-def add():
-    number = random.randint(1,5)
-    return number
+import os
+import sys
+import time
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../../')))
+from sphero_sdk import SpheroRvrAsync
+from sphero_sdk import SerialAsyncDal
 
-#display.print(add())
-sum = add() + 3
-print(sum)
-print(type(add()))
+import asyncio
+
+loop = asyncio.get_event_loop()
+rvr = SpheroRvrAsync(
+    dal=SerialAsyncDal(
+        loop
+    )
+)
+
+async def main():
+    await rvr.wake()
+    await rvr.drive_with_heading(30,0,2)
+
+try:
+    loop.run_until_complete(
+        asyncio.gather(
+            main()
+        )
+    )
+except:
+    print("error")
