4cf254a178aaa1af2679c286eae9a22da0ce5911
odaadvanced <stbaoda@gmail.com>
2023-05-10 22:54:58 -0500

    message

--- Jedidiah/final_project.py
+++ Jedidiah/final_project.py
@@ -27,13 +27,13 @@ rvr = SpheroRvrAsync(
 )
 p = PiAnalog()
 p2 = PiAnalogThermistor()
-multiplier = 2000
+multiplier = 2000                     #Used for phototransistor: increase to make detections more sensitive
 detection_file_path = Path.home()/'color_data.txt'
 other_detection_file_path = Path.home()/'immediate_color_data.txt'
 
-GPIO.setmode(GPIO.BCM)
+GPIO.setmode(GPIO.BCM)              #Broadcom mode
 
-right_trigger = 20
+right_trigger = 20     #Setting up GPIO pins for the ultrasonic sensors.
 right_echo = 21
 left_trigger = 27
 left_echo = 24
@@ -44,7 +44,7 @@ GPIO.setup(right_trigger, GPIO.OUT)
 GPIO.setup(right_echo, GPIO.IN)
 
 def display_speed():
-    speed2 = random.randint(10,30)    
+    speed2 = random.randint(10,30)    #Speed of rover is number between 10 and 30 (possible range is 0-255).
     return speed2
 
 def detect_light():
@@ -60,7 +60,7 @@ def detect_temp():
 def detect_color():
     global other_detection_file_path
     with open(other_detection_file_path, mode= 'r', encoding = 'utf-8') as file:
-        text = file.read()        
+        text = file.read()            #Reads information about color detection from immediate_color_data.txt
         list_text = text.split(", ")
     return list_text
             
@@ -75,16 +75,15 @@ async def main():
     # Give RVR time to wake up
         await asyncio.sleep(2)
         with detection_file_path.open(mode = 'w', encoding = 'utf-8') as file:
-            file.write('')
+            file.write('')                                #Writes information from color_data.txt
         await rvr.enable_color_detection(is_enabled=True)
         await rvr.sensor_control.add_sensor_data_handler(
             service=RvrStreamingServices.color_detection,
             handler=color_detected_handler
         )
         await rvr.sensor_control.start(interval=250)
-
-        await asyncio.sleep(1)
         color_changed(detect_color()[0], detect_color()[1], detect_color()[2])
+        await asyncio.sleep(1)
         new_speed = display_speed()
         dist_r = distance_right()
         dist_l = distance_left()
@@ -113,13 +112,9 @@ async def main():
         elif dist_l >= 50 and dist_r >= 50:
             display.print(str(new_speed))        
             await rvr.drive_with_heading(new_speed,0,2)
-            await asyncio.sleep(4)
+            await asyncio.sleep(2)
  
-try:    
-#     asyncio.ensure_future(
-#         main()
-#     )
-#     loop.run_forever()
+try:
     loop.run_until_complete(
         asyncio.gather(
             main()
@@ -130,7 +125,6 @@ except KeyboardInterrupt:
     print('Program terminated by keyboard interrupt.')
     GPIO.cleanup()
 
-finally:
-    #rvr.sensor_control.clear()
+finally:    
     time.sleep(.5)    
     rvr.close()    
