7e124ad9af6acd67498234f60120ea939dc62b85
odaadvanced <stbaoda@gmail.com>
2023-05-23 18:55:16 -0500

    message

--- Jedidiah/test.py
+++
@@ -1,6 +0,0 @@
-import espeak
-espeak.init()
-speaker = espeak.Espeak()
-speaker.say("Hello")
-speaker.rate = 300
-speaker.say("Hello again")
--- Jedidiah/testing.py
+++
@@ -1,4 +0,0 @@
-import subprocess
-
-sound_file = '/home/pi/dev/hutto/Jedidiah/output.mp3'
-subprocess.call(['xdg-open', sound_file])
