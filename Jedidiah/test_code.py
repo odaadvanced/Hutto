d8627ca5068ffb32bb9f24bf6fa58e9bb1cc91af
odaadvanced <stbaoda@gmail.com>
2023-05-23 09:01:26 -0500

    message

--- Jedidiah/test_code.py
+++ Jedidiah/test_code.py
@@ -1,28 +1,26 @@
+from gtts import gTTS
 import os
-import sys
-import time
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../../')))
-from sphero_sdk import SpheroRvrAsync
-from sphero_sdk import SerialAsyncDal
 
-import asyncio
+myText = '''We have just landed on Mars. This place is cold and bizarre.
+We are searching for signs of life. Can one stand here alive?
+The temperature here is mild. The number of obstacles- wild.
+This place is surprisingly bright. Its rather flooded with light.
+The ground beneath is rather gray, The horizons blue one way.
+The other way its sickly green. What this means is yet unseen.
+Its something I shall ponder. While this vast terrain I wander.
+On this journey my sole object. Is to help three kids on their project.
+Tell those earthlings down below. They can speed here- no one will know.
+Not that I can really speed, I am just a rover I concede.
+I will capture all I can, I will take a shot at that man,
+And send it all down to earth- To that blazing Texas herth.
+This place is dreary and bleak. I hear my tires squeak.
+But other than that. Theres no sound on this flat.
+I quietly creep like a cat. But wait! What noise was that?
+Perhaps some squeaking of shoes? Oh, danger overhead brews!
+Does Mars hold aliens? Or worse yet- dreadful humans?'''
 
-loop = asyncio.get_event_loop()
-rvr = SpheroRvrAsync(
-    dal=SerialAsyncDal(
-        loop
-    )
-)
+language = 'en'
 
-async def main():
-    await rvr.wake()
-    await rvr.drive_with_heading(30,0,2)
+output = gTTS(text = myText, lang = language, slow = False)
 
-try:
-    loop.run_until_complete(
-        asyncio.gather(
-            main()
-        )
-    )
-except:
-    print("error")
+output.save("output.mp3")
--- Jedidiah/testing.py
+++ Jedidiah/testing.py
@@ -1,11 +1,4 @@
-list_dict = [False, False, True, False]
-for j in range(1, 5):
-    for k in range(4):
-        if (k+1) % j == 0:
-            if list_dict[k] == False:
-                list_dict[k] = True
-            else:
-                list_dict[k] = False
-        else:
-            print((k+1) % j)
-print(list_dict)
+import subprocess
+
+sound_file = '/home/pi/dev/hutto/Jedidiah/output.mp3'
+subprocess.call(['xdg-open', sound_file])
